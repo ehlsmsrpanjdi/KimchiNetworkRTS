a94b3437d05ba7dd384f8f976fd26c1463e91790
ehlsmsrpanjdi <usm9401@gmail.com>
2026-01-17 17:44:52 +0900

    ui기초 생성중

---
+++ Assets/Script/UI/GameStateUI/GameStateButtonUI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameStateButtonUI : UIBase
+{
+    [SerializeField] Button button;
+    GameStatePanelUI panelUI;
+
+    private void Reset()
+    {
+        button = GetComponent<Button>();
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+
+        panelUI = UIManager.Instance.GetUI<GameStatePanelUI>();
+
+        button.onClick.AddListener(() =>
+        {
+            panelUI.Toggle();
+        });
+    }
+}
--- Assets/Script/UI/GameStateUI/GameStatePanelUI.cs
+++ Assets/Script/UI/GameStateUI/GameStatePanelUI.cs
@@ -1,12 +1,12 @@
 ﻿using DG.Tweening;
+using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
 
-public class BuildingSystemUI : UIBase
+public class GameStatePanelUI : UIBase
 {
     [Header("Refs")]
     [SerializeField] RectTransform panel; // 본체
-    [SerializeField] RectTransform button; // 슬라이드 버튼 (BuildingSystemButtonUI)
-    [SerializeField] RectTransform resourceUI; // 리소스 UI
+    [SerializeField] RectTransform button; // 슬라이드 버튼 (GameStateButtonUI)
 
     [Header("Tween")]
     [SerializeField] float duration = 0.3f;
@@ -27,8 +27,7 @@ public class BuildingSystemUI : UIBase
         // 높이는 패널 기준
         height = panel.rect.height;
 
-        button = UIManager.Instance.GetUI<BuildingSystemButtonUI>().GetComponent<RectTransform>();
-        resourceUI = UIManager.Instance.GetUI<BuildingResourceUI>().GetComponent<RectTransform>();
+        button = UIManager.Instance.GetUI<GameStateButtonUI>().GetComponent<RectTransform>();
 
         // 시작 위치: 둘 다 height만큼 아래
         SetHiddenImmediate();
@@ -38,13 +37,10 @@ public class BuildingSystemUI : UIBase
     {
         // 즉시 위치 세팅 (트윈 없이)
         panel.anchoredPosition =
-            new Vector2(panel.anchoredPosition.x, -height);
+            new Vector2(panel.anchoredPosition.x, height);
 
         button.anchoredPosition =
             new Vector2(button.anchoredPosition.x, 0);
-
-        resourceUI.anchoredPosition =
-    new Vector2(button.anchoredPosition.x, 0);
 
         isOpen = false;
     }
@@ -54,11 +50,10 @@ public class BuildingSystemUI : UIBase
         // 기존 트윈 제거 (현재 위치 기준으로 새로 시작)
         DOTween.Kill(panel);
         DOTween.Kill(button);
-        DOTween.Kill(resourceUI);
 
-        float targetY = isOpen ? -height : 0f;
+        float targetY = isOpen ? height : 0f;
 
-        float buttonTargetY = targetY + height;
+        float buttonTargetY = targetY - height;
 
         isOpen = !isOpen;
 
@@ -70,9 +65,5 @@ public class BuildingSystemUI : UIBase
         button.DOAnchorPosY(buttonTargetY, duration)
               .SetEase(ease)
               .SetTarget(button);
-
-        resourceUI.DOAnchorPosY(buttonTargetY, duration)
-              .SetEase(ease)
-              .SetTarget(resourceUI);
     }
 }
