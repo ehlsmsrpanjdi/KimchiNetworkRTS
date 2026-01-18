5594f29519a326ba090440b7b8c53f5ddfd734c2
ehlsmsrpanjdi <usm9401@gmail.com>
2026-01-18 16:49:39 +0900

    에셋넣고 클래스추가좀함

---
+++ Assets/Script/UI/BuildingSystemUI/Element/BuildingButtonsUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildingButtonsUI : UIBase
+{
+    [SerializeField] GridLayoutGroup gridLayoutGroup;
+    [SerializeField] GameObject buildingButtonUIPrefab;
+    [SerializeField] List<GameObject> buildingButtonUIs = new List<GameObject>();
+
+
+    private void Reset()
+    {
+        gridLayoutGroup = GetComponent<GridLayoutGroup>();
+        buildingButtonUIPrefab = Resources.Load<GameObject>("BuildingButton");
+    }
+
+
+}
---
+++ Assets/Script/UI/BuildingSystemUI/Element/BuildingElementUI.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildingElementUI : UIBase
+{
+    [SerializeField] Button battleBuildingButton;
+    [SerializeField] Button resourceBuildingButton;
+    [SerializeField] Button wallBuildingButton;
+
+    BuildingButtonsUI buildingButtonsUI;
+
+    private void Reset()
+    {
+        battleBuildingButton = this.TryFindChild("BattleButton").GetComponent<Button>();
+        resourceBuildingButton = this.TryFindChild("ResourceButton").GetComponent<Button>();
+        wallBuildingButton = this.TryFindChild("WallButton").GetComponent<Button>();
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+
+        battleBuildingButton.onClick.AddListener(OnClickBattle);
+        resourceBuildingButton.onClick.AddListener(OnClickResource);
+        wallBuildingButton.onClick.AddListener(OnClickWall);
+
+        buildingButtonsUI = UIManager.Instance.GetUI<BuildingButtonsUI>();
+    }
+
+    void OnClickBattle()
+    {
+        //buildingButtonsUI
+
+    }
+
+    void OnClickResource()
+    {
+        //buildingButtonsUI
+
+    }
+
+    void OnClickWall()
+    {
+        //buildingButtonsUI
+
+    }
+}
